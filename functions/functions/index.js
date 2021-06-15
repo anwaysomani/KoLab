060fbfed165f69c84d057ad5cd5a90f55511e379
Anway <31155434+anwaysomani@users.noreply.github.com>
2021-06-15 22:05:15 +0530

    Updated functions

--- functions/functions/index.js
+++ functions/functions/index.js
@@ -1,17 +1,40 @@
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
 const cors = require("cors")({origin: true});
-
+const nodemailer = require("nodemailer");
 admin.initializeApp();
 
 const auth = admin.auth();
 
+// const employeeHours = 1; /* constant employee hours */
+
+const clientName = 'KoLab';
+
+const mailConfig = {
+  mail: '',
+  pass: '',
+  fromConf: 'Anway Somani <anwaysomani@gmail.com>'
+};
+
+const transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  secure: false,
+  auth: {
+    type: 'LOGIN',
+    user: mailConfig.mail,
+    pass: mailConfig.pass,
+  },
+});
+
+/* get user listing */
 module.exports.getAllUsers = functions.https.onRequest((req, res) => {
   cors(req, res, () => {
     const arr = [];
+    // eslint-disable-next-line max-len
     admin.firestore().collection("users").where("designation", "==", req.body.designation)
       .get().then((data) => {
-      for (var i in data.docs) {
+      // eslint-disable-next-line guard-for-in
+      for (const i in data.docs) {
         arr.push(data.docs[i].data());
       }
       res.send(arr);
@@ -42,20 +65,65 @@ module.exports.addUser = functions.https.onRequest((req, res) => {
           uid: uid,
           email: req.body.email,
           name: req.body.name,
+          mobile: req.body.mobile,
           isNewLogin: true,
-          disable: true,
           attendance: [],
           currentStatus: "Sign Out",
+          totalTimeToday: 0,
         }).then(() => {
-          res.sendStatus(200);
+          auth.generatePasswordResetLink(req.body.email).then((link) => {
+            transporter.sendMail({
+              from: mailConfig.fromConf,
+              to: req.body.email,
+              subject: "Reset your password for " + clientName,
+              // eslint-disable-next-line max-len
+              html: "<p>You can reset password using the following link:</p><a>" + link + "</a>",
+            }, (erro, info) => {
+              if (erro) {
+                res.sendStatus(400);
+              }
+              res.sendStatus(200);
+            });
+          });
         }, () => {
           res.sendStatus(409);
         });
       }, (error) => {
-        res.statusMessage("Duplicate record entry attempted");
         res.sendStatus(412);
-        res.send();
       });
     });
   });
 });
+
+/* revert status of all employees to sign out */
+module.exports.signOutAllEmployees = functions.https.onRequest((req, res) => {
+  cors(req, res, () => {
+    admin.firestore().collection("users").get().then((data) => {
+      const currTime = new Date();
+      const time = currTime.getHours() + ":" + currTime.getMinutes();
+      for (const i in data.docs) {
+        if (data.docs[i].data().currentStatus === "Sign In") {
+          // eslint-disable-next-line max-len
+          const lastSignInRecordTime = data.docs[i].data().attendance[data.docs[i].data().attendance.length - 1].time.split(":")[0];
+          const currentTime = (new Date()).getHours();
+          // eslint-disable-next-line max-len
+          const calcTime = data.docs[i].data().totalTimeToday + (currentTime - lastSignInRecordTime);
+          const x = {
+            date: currTime.getDate(),
+            time,
+            site: data.docs[i].data().activeSite,
+            status: "Sign Out",
+            reason: "",
+          };
+
+          data.docs[i].data().attendance.push(x);
+          data.docs[i].ref.update({
+            totalTimeToday: calcTime,
+            attendance: data.docs[i].data().attendance,
+          });
+        }
+      }
+    });
+  });
+});
+
