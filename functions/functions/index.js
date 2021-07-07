1ff6c1ac510d0611bc37c3370e034a22d18e6e29
guptarajiv535 <guptarajiv535@gmail.com>
2021-07-07 18:20:28 +0530

    Updated reports function to add data.

--- functions/functions/index.js
+++ functions/functions/index.js
@@ -1,6 +1,6 @@
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
-const cors = require("cors")({origin: true});
+const cors = require("cors")({ origin: true });
 const nodemailer = require("nodemailer");
 admin.initializeApp();
 
@@ -8,19 +8,19 @@ const auth = admin.auth();
 
 // const employeeHours = 1; /* constant employee hours */
 
-const clientName = 'KoLab';
+const clientName = "KoLab";
 
 const mailConfig = {
-  mail: '',
-  pass: '',
-  fromConf: 'Anway Somani <anwaysomani@gmail.com>'
+  mail: "",
+  pass: "",
+  fromConf: "Anway Somani <anwaysomani@gmail.com>",
 };
 
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
   secure: false,
   auth: {
-    type: 'LOGIN',
+    type: "LOGIN",
     user: mailConfig.mail,
     pass: mailConfig.pass,
   },
@@ -31,14 +31,18 @@ module.exports.getAllUsers = functions.https.onRequest((req, res) => {
   cors(req, res, () => {
     const arr = [];
     // eslint-disable-next-line max-len
-    admin.firestore().collection("users").where("designation", "==", req.body.designation)
-      .get().then((data) => {
-      // eslint-disable-next-line guard-for-in
-      for (const i in data.docs) {
-        arr.push(data.docs[i].data());
-      }
-      res.send(arr);
-    });
+    admin
+      .firestore()
+      .collection("users")
+      .where("designation", "==", req.body.designation)
+      .get()
+      .then((data) => {
+        // eslint-disable-next-line guard-for-in
+        for (const i in data.docs) {
+          arr.push(data.docs[i].data());
+        }
+        res.send(arr);
+      });
   });
 });
 
@@ -47,50 +51,70 @@ module.exports.addUser = functions.https.onRequest((req, res) => {
   cors(req, res, () => {
     auth.listUsers().then((userRecords) => {
       const uid = "JCP" + (userRecords["users"].length + 1);
-      auth.createUser({
-        uid: uid,
-        email: req.body.email,
-        displayName: req.body.name,
-        password: req.body.name.split(" ")[0].toLowerCase() + "123",
-      }).then(() => {
-        admin.firestore().doc(`/users/${uid}`).set({
-          designation: req.body.designation,
-          activeSite: {
-            site: "",
-            client: "",
-            address: "",
-            pincode: "",
-          },
-          sites: [],
+      auth
+        .createUser({
           uid: uid,
           email: req.body.email,
-          name: req.body.name,
-          mobile: req.body.mobile,
-          isNewLogin: true,
-          attendance: [],
-          currentStatus: "Sign Out",
-          totalTimeToday: 0,
-        }).then(() => {
-          auth.generatePasswordResetLink(req.body.email).then((link) => {
-            transporter.sendMail({
-              from: mailConfig.fromConf,
-              to: req.body.email,
-              subject: "Reset your password for " + clientName,
-              // eslint-disable-next-line max-len
-              html: "<p>You can reset password using the following link:</p><a>" + link + "</a>",
-            }, (erro, info) => {
-              if (erro) {
-                res.sendStatus(400);
-              }
-              res.sendStatus(200);
-            });
-          });
-        }, () => {
-          res.sendStatus(409);
-        });
-      }, (error) => {
-        res.sendStatus(412);
-      });
+          displayName: req.body.name,
+          password: req.body.name.split(" ")[0].toLowerCase() + "123",
+        })
+        .then(
+          () => {
+            admin
+              .firestore()
+              .doc(`/users/${uid}`)
+              .set({
+                designation: req.body.designation,
+                activeSite: {
+                  site: "",
+                  client: "",
+                  address: "",
+                  pincode: "",
+                },
+                sites: [],
+                uid: uid,
+                email: req.body.email,
+                name: req.body.name,
+                mobile: req.body.mobile,
+                isNewLogin: true,
+                attendance: [],
+                currentStatus: "Sign Out",
+                totalTimeToday: 0,
+              })
+              .then(
+                () => {
+                  auth
+                    .generatePasswordResetLink(req.body.email)
+                    .then((link) => {
+                      transporter.sendMail(
+                        {
+                          from: mailConfig.fromConf,
+                          to: req.body.email,
+                          subject: "Reset your password for " + clientName,
+                          // eslint-disable-next-line max-len
+                          html:
+                            "<p>You can reset password using the following link:</p><a>" +
+                            link +
+                            "</a>",
+                        },
+                        (erro, info) => {
+                          if (erro) {
+                            res.sendStatus(400);
+                          }
+                          res.sendStatus(200);
+                        }
+                      );
+                    });
+                },
+                () => {
+                  res.sendStatus(409);
+                }
+              );
+          },
+          (error) => {
+            res.sendStatus(412);
+          }
+        );
     });
   });
 });
@@ -134,13 +158,14 @@ module.exports.attendanceRegularize = functions.https.onRequest((req, res) => {
                 })
                 .then(() => {
                   res.send(200, {
-                    "message": "Document successfully updated"
-                })
+                    message: "Document successfully updated",
+                  });
                 })
                 .catch((error) => {
                   res.send(500, {
-                    "message": "Error updating document:", error
-                })
+                    message: "Error updating document:",
+                    error,
+                  });
                 });
             }
           }
@@ -155,55 +180,109 @@ module.exports.attendanceRegularize = functions.https.onRequest((req, res) => {
       });
   });
 });
-  
+
 /* revert status of all employees to sign out */
 module.exports.signOutAllEmployees = functions.https.onRequest((req, res) => {
   cors(req, res, () => {
-    admin.firestore().collection("users").get().then((data) => {
-      const currTime = new Date();
-      const time = currTime.getHours() + ":" + currTime.getMinutes();
-      for (const i in data.docs) {
-        if (data.docs[i].data().currentStatus === "Sign In") {
-          // eslint-disable-next-line max-len
-          const lastSignInRecordTime = data.docs[i].data().attendance[data.docs[i].data().attendance.length - 1].time.split(":")[0];
-          const currentTime = (new Date()).getHours();
-          // eslint-disable-next-line max-len
-          const calcTime = data.docs[i].data().totalTimeToday + (currentTime - lastSignInRecordTime);
-          const x = {
-            date: currTime.getDate(),
-            time,
-            site: data.docs[i].data().activeSite,
-            status: "Sign Out",
-            reason: "",
-          };
+    admin
+      .firestore()
+      .collection("users")
+      .get()
+      .then((data) => {
+        const currTime = new Date();
+        const time = currTime.getHours() + ":" + currTime.getMinutes();
+        for (const i in data.docs) {
+          if (data.docs[i].data().currentStatus === "Sign In") {
+            // eslint-disable-next-line max-len
+            const lastSignInRecordTime = data.docs[i]
+              .data()
+              .attendance[data.docs[i].data().attendance.length - 1].time.split(
+                ":"
+              )[0];
+            const currentTime = new Date().getHours();
+            // eslint-disable-next-line max-len
+            const calcTime =
+              data.docs[i].data().totalTimeToday +
+              (currentTime - lastSignInRecordTime);
+            const x = {
+              date: currTime.getDate(),
+              time,
+              site: data.docs[i].data().activeSite,
+              status: "Sign Out",
+              reason: "",
+            };
 
-          data.docs[i].data().attendance.push(x);
-          data.docs[i].ref.update({
-            totalTimeToday: calcTime,
-            attendance: data.docs[i].data().attendance,
-          });
+            data.docs[i].data().attendance.push(x);
+            data.docs[i].ref.update({
+              totalTimeToday: calcTime,
+              attendance: data.docs[i].data().attendance,
+            });
+          }
         }
-      }
-    });
+      });
   });
 });
 
 module.exports.supervisorReports = functions.https.onRequest((req, res) => {
   cors(req, res, () => {
     let employee = {};
+	let sites =[];
     admin
       .firestore()
       .collection("users")
       .get()
       .then((data) => {
+        var allEmployees = [];
         // eslint-disable-next-line guard-for-in
         for (const i in data.docs) {
-            employee = data.docs[i].data().attendance.filter((item) => {
-            return item.date == req.body.date && item.site.site==req.body.site.site && item.site.client==req.body.site.client;
+          var emp = data.docs[i].data();
+          employee = emp.attendance.filter((item) => {
+            return (
+              item.date == req.body.date &&
+              item.site.site == req.body.site.site &&
+              item.site.client == req.body.site.client
+            );
           });
+          if (employee.length > 0) {
+            allEmployees.push({
+              name: emp.name,
+              attendance: employee,
+              designation: emp.designation,
+            });
+          }
         }
-        /* admin.firestore().collection("reports").add(employee); */
-        res.send(employee);
+		let clientsCollection = admin.firestore().collection("clients");
+		clientsCollection.get().then(client=>{
+			for (const j in client.docs) {
+				sites = client.docs[j].data().sites.filter(site=>{
+					return site;
+				});
+			}
+		})
+        let reportsCollection = admin.firestore().collection("reports");
+        reportsCollection.add({
+          siteName: req.body.site.site,
+          date: req.body.date,
+          clientName: req.body.site.client,
+          reportType: "A",
+          data: {
+            employee: allEmployees.filter(data=>{
+                return data.designation=='Employee';
+            }),
+            supervisor: allEmployees.filter(data=>{
+                return data.designation=='Supervisor';
+            }),
+            contractor: sites.filter(data=>{
+                return data.contractors;
+            }),
+            visitor: sites.filter(data=>{
+                return data.visitors;
+            }),
+            workProgress: [],
+            material: [],
+          },
+        });
+        res.send(allEmployees);
       })
       .catch((error) => {
         console.log("Error getting documents: ", error);
